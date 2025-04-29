f102afc93a2423385940032ddb142683ce7e6cd3
LeeTor4 <leetor4@gmail.com>
2025-04-29 08:55:27 -0300

    Implementando Repository e testando o banco

--- rest-with-spring-boot-and-java-erudio-2025/src/main/java/br/com/erudio/services/PersonServices.java
+++ rest-with-spring-boot-and-java-erudio-2025/src/main/java/br/com/erudio/services/PersonServices.java
@@ -1,6 +1,9 @@
 package br.com.erudio.services;
 
+import br.com.erudio.exception.ResourceNotFoundException;
 import br.com.erudio.model.Person;
+import br.com.erudio.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -12,58 +15,54 @@ import java.util.logging.Logger;
 public class PersonServices {
 
     private final AtomicLong counter = new AtomicLong();
-
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    @Autowired
+    private PersonRepository repository;
 
     public List<Person> findALl(){
         logger.info("FindAll Person!");
-        List<Person> persons = new ArrayList<Person>();
-        for(int i = 0; i < 9 ; i++){
-            Person person = mockPerson(i);
-            persons.add(person);
-        }
 
-        return persons;
+        return repository.findAll();
     }
 
-    public Person findById(String id) {
+    public Person findById(Long id) {
         logger.info("Finding one Person!");
 
-        Person person = new Person();
-        person.setId(counter.incrementAndGet());
-        person.setFirstName("Leandro");
-        person.setLastName("Costa");
-        person.setAddress("Uberlândia - Minas Gerais - Brasil");
-        person.setGender("Male");
-        return person;
+        return repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for ID!"));
     }
 
     public Person create(Person person){
 
         logger.info("Create one Person!");
-        return person;
+        return repository.save(person);
     }
 
     public Person update(Person person) {
 
         logger.info("Updating one Person!");
 
-        return person;
+       Person entity = repository.findById(person.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for ID!"));
+
+       entity.setFirstName(person.getFirstName());
+       entity.setLastName(person.getLastName());
+       entity.setAddress(person.getAddress());
+       entity.setGender(person.getGender());
+
+        return repository.save(person);
     }
 
-    public void delete(String id) {
+    public void delete(Long id) {
 
         logger.info("Deleting one Person!");
 
+        Person entity = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for ID!"));
+
+        repository.delete(entity);
     }
-    private Person mockPerson(int i) {
-        Person person = new Person();
-        person.setId(counter.incrementAndGet());
-        person.setFirstName("FirstName " + i);
-        person.setLastName("LastName " + i);
-        person.setAddress("Some addres in  Brasil");
-        person.setGender("Male");
-        return person;
-    }
+
 
 }
