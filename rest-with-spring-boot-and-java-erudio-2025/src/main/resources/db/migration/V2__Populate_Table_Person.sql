413fdca8082d99420cf88c490eeb21eed9357537
LeeTor4 <leetor4@gmail.com>
2025-05-15 07:43:31 -0300

    Impl Flayway

---
+++ rest-with-spring-boot-and-java-erudio-2025/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,4 @@
+INSERT INTO `person` VALUES (1,'Raquel','Alves','Fortaleza - CE','Female');
+INSERT INTO `person` VALUES (2,'Lee','Torquato','Fortaleza - Ceara - Brasil','Male');
+INSERT INTO `person` VALUES (4,'Miguel','Aragão','Fortaleza - Ceara - Brasil','Male');
+INSERT INTO `person` VALUES (6,'Sofia','Mehmood','Fortaleza - Ceara - Brasil','Male');
