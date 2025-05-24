7e8fcd3c4d16a6a7a85abb23eefad64cdcdc79bf
LeeTor4 <leetor4@gmail.com>
2025-05-24 15:33:43 -0300

    Customizando o Json Serialization

--- rest-with-spring-boot-and-java-erudio-2025/src/main/java/br/com/erudio/data/dto/PersonDTO.java
+++ rest-with-spring-boot-and-java-erudio-2025/src/main/java/br/com/erudio/data/dto/PersonDTO.java
@@ -1,11 +1,14 @@
 package br.com.erudio.data.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
 import java.util.Objects;
 
-
+@JsonPropertyOrder({"id","address","first_name","last_name","gender"})
 public class PersonDTO implements Serializable {
 
      private static final Long serialVersionID = 1L;
@@ -13,12 +16,13 @@ public class PersonDTO implements Serializable {
 
      private Long id;
 
+     @JsonProperty("first_name")
      private String firstName;
-
+     @JsonProperty("last_name")
      private String lastName;
 
      private String address;
-
+     @JsonIgnore
      private String gender;
 
 
